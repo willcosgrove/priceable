449bb77808e535cb6a4367fed7684adc99e81a01
Will Cosgrove <will@willcosgrove.com>
2019-08-17 00:34:26 -0500

    Remove attr_accessible and tidy up

--- lib/priceable.rb
+++ lib/priceable.rb
@@ -1,29 +1,27 @@
+# frozen_string_literal: true
+
 require "priceable/version"
 
 module Priceable
-  SUFFIXES = ["_in_cents", "_in_pennies", "_as_integer"]
+  SUFFIXES = ["_in_cents", "_in_pennies", "_as_integer"].freeze
+  
   def priceable(*price_fields)
     price_fields.each do |price_field|
-      suffix = SUFFIXES.detect { |suffix| self.attribute_method? "#{price_field}#{suffix}".to_sym }
+      suffix = SUFFIXES.detect { |suffix| attribute_method? "#{price_field}#{suffix}" }
       raise ArgumentError, "Unable to find valid database field for `#{price_field}'" unless suffix
+      
       define_method price_field do
-        unless send("#{price_field}#{suffix}".to_sym).nil?
-          send("#{price_field}#{suffix}".to_sym) / 100.0
+        unless send("#{price_field}#{suffix}").nil?
+          send("#{price_field}#{suffix}") / 100.0
         else
           0.0
         end
       end
-      define_method "#{price_field}=".to_sym do |new_price|
-        send("#{price_field}#{suffix}=".to_sym, (new_price.to_f * 100).round)
+      
+      define_method "#{price_field}=" do |new_price|
+        send("#{price_field}#{suffix}=", (new_price.to_f * 100).round)
       end
     end
-
-    unless Rails::VERSION::MAJOR == 4 && !defined?(ProtectedAttributes)
-      if self._accessible_attributes?
-        attr_accessible *price_fields
-      end
-    end
-
   end
 end
 
